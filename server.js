97506bc2676aab161065f17f15d0e2fc3317f962
Suyash Mishra <121488134+suy485mishra@users.noreply.github.com>
2023-12-28 19:26:57 +0530

    done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import Product from "./models/productModel.js";
 const app = express();
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 //getall products
 app.get("/", async (req, res) => {
@@ -23,11 +24,10 @@ app.get("/blog", (req, res) => {
   res.send("heya i am kuber");
 });
 
-
 //fetch by id
 app.get("/products/:id", async (req, res) => {
   try {
-    const {id}=req.params;
+    const { id } = req.params;
     const product = await Product.findById(id);
     res.status(200).json(product);
   } catch (error) {
@@ -47,17 +47,39 @@ app.post("/products", async (req, res) => {
   }
 });
 
-//update 
-app.put("/products/:id",async(req,res)=>{
-    try {
-        const {id}=req.params;
-    } catch (error) {
-        res.status(500).json({ message: "sb not changa si" });
+//update
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
     }
-})
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: "sb not changa si" });
+  }
+});
+// delete a product
 
-
-
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 mongoose
   .connect(
     "mongodb+srv://suyash:7jIrpLn5LZRAr4aX@cluster0.t65usjp.mongodb.net/restful?retryWrites=true&w=majority"
